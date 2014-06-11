5803e5ed2bd1e01d0507617f01c803f8f22a4ae0
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-06-11 19:59:09 +0400

    Remove standard headers

--- Source/examples/padded.cpp
+++ Source/examples/padded.cpp
@@ -1,17 +1,14 @@
-#include <cstdint> // uint8_t
-#include <iostream> // std::cout
-
 #include <boost/config.hpp>
 
 struct Foo {
-  uint8_t x;
+  char x;
 #include <leathers/push>
 #include <leathers/padded>
-  uint64_t y;
+  long y;
 #include <leathers/pop>
 };
 
 int main() {
   // struct Foo must be used, otherwise there will be no warning (clang)
-  std::cout << Foo().x << std::endl;
+  return static_cast<int>(Foo().y);
 }
