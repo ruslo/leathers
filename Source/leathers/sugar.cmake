ef71ce081426a92ef00725ecc9c7c3e988f6c10b
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-06-15 17:31:25 +0400

    Update

--- Source/leathers/sugar.cmake
+++ Source/leathers/sugar.cmake
@@ -1,7 +1,8 @@
-# This file generated automatically by:
-#   generate_sugar_files.py
-# see wiki for more info:
-#   https://github.com/ruslo/sugar/wiki/Collecting-sources
+# This file generated automatically:
+# https://github.com/ruslo/sugar/wiki/Cross-platform-warning-suppression
+
+# Copyright (c) 2014, Ruslan Baratov
+# All rights reserved.
 
 if(DEFINED LEATHERS_SOURCE_LEATHERS_SUGAR_CMAKE_)
   return()
@@ -17,32 +18,76 @@ sugar_files(
     push
     pop
     ###
-    cast_align
+    all
+    ###
+    c++98-compat
+    c++98-compat-pedantic
+    cast-align
+    conditional-uninitialized
     conversion
+    covered-switch-default
     deprecated
-    direct_ivar_access
-    disabled_macro_expansion
-    enum_not_handled_in_switch
-    exit_time_destructors
-    explicit_ownership_type
-    extended_offsetof
-    format_nonliteral
-    global_constructors
-    gnu
-    implicit_fallthrough
-    missing_prototypes
-    missing_variable_declarations
-    over_aligned
+    deprecated-declarations
+    deprecated-objc-isa-usage
+    deprecated-register
+    disabled-macro-expansion
+    documentation
+    documentation-unknown-command
+    empty-body
+    extra-semi
+    global-constructors
+    implicit-fallthrough
+    four-char-constants
+    missing-noreturn
+    non-virtual-dtor
+    old-style-cast
     padded
-    pedantic
+    shift-sign-overflow
+    sign-compare
+    switch
+    switch-enum
+    undef
+    unreachable-code
+    unused-parameter
+    used-but-marked-unused
+    weak-vtables
+    shadow
+    bool-conversion
+    constant-conversion
+    shorten-64-to-32
+    enum-conversion
+    int-conversion
+    sign-conversion
+    missing-braces
+    return-type
+    parentheses
+    missing-field-initializers
+    missing-prototypes
+    newline-eof
+    pointer-sign
+    format
+    uninitialized
+    unknown-pragmas
+    unused-function
+    unused-label
+    unused-value
+    unused-variable
+    exit-time-destructors
+    overloaded-virtual
+    invalid-offsetof
+    c++11-extensions
+    duplicate-method-match
+    implicit-atomic-properties
+    objc-missing-property-synthesis
+    protocol
     selector
-    sign_conversion
-    unreachable_code
-    unused_function
-    unused_macros
-    unused_parameter
-    unused_variable
-    used_but_marked_unused
-    weak_template_vtables
-    weak_vtables
+    deprecated-implementations
+    strict-selector-match
+    undeclared-selector
+    objc-root-class
+    explicit-ownership-type
+    implicit-retain-self
+    arc-repeated-use-of-weak
+    receiver-is-weak
+    arc-bridge-casts-disallowed-in-nonarc
 )
