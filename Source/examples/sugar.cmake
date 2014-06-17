9eb0f921135d50fc8c978e46a87afabbc7236349
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-06-17 14:20:02 +0400

    Add new examples to sugar.cmake

--- Source/examples/sugar.cmake
+++ Source/examples/sugar.cmake
@@ -13,30 +13,73 @@ include(sugar_files)
 
 sugar_files(
     LEATHERS_EXAMPLES_SOURCES
+    # compatibility-c++98 (see one more below)
     c++98-compat-pedantic.cpp
-    c++98-compat.cpp
+
+    # special-members
+    assign-base-inaccessible.cpp
+    assign-could-not-be-generated.cpp
+    copy-ctor-could-not-be-generated.cpp
+    dflt-ctor-base-inaccessible.cpp
+    dflt-ctor-could-not-be-generated.cpp
+    user-ctor-required.cpp
+
+    # inline
+    automatic-inline.cpp
+    force-not-inlined.cpp
+    not-inlined.cpp
+    unreferenced-inline.cpp
+
+    #
+    behavior-change.cpp
     cast-aligned.cpp
+    catch-semantic-changed.cpp
     conditional-uninitialized.cpp
+    constant-conditional.cpp
+    conversion-loss-return.cpp
+    conversion-loss.cpp
     conversion.cpp
-    covered-switch-default
+    covered-switch-default.cpp
     deprecated-declarations.cpp
     deprecated-register.cpp
     deprecated.cpp
+    digraphs-not-supported.cpp
     documentation-unknown-command.cpp
     documentation.cpp
+    expression-has-no-effect.cpp
     extra-semi.cpp
     global-constructors.cpp
+    ill-formed-comma-expr.cpp
+    inherits-via-dominance.cpp
+    is-defined-to-be.cpp
+    layout-changed.cpp
     missing-noreturn.cpp
+    name-length-exceeded.cpp
+    no-such-warning.cpp
     non-virtual-dtor.cpp
+    object-layout-change.cpp
     old-style-cast.cpp
     padded.cpp
     shift-sign-overflow.cpp
     sign-compare.cpp
+    signed-unsigned-compare.cpp
+    signed-unsigned-mismatch.cpp
+    static-ctor-not-thread-safe.cpp
     switch-enum.cpp
     switch.cpp
+    this-used-in-init.cpp
     undef.cpp
+    unreachable-code-simple.cpp
     unreachable-code.cpp
+    unsafe-conversion.cpp
     unused-parameter.cpp
     used-but-marked-unused.cpp
     weak-vtables.cpp
 )
+
+if(NOT MSVC)
+  sugar_files(
+    LEATHERS_EXAMPLES_SOURCES
+    c++98-compat.cpp # inline namespaces is not supported
+  )
+endif()
