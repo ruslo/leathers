19e1c48bbb5ad3f1e1a00f14e4a7dd34ba48a279
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-06-18 00:06:16 +0400

    Rename

--- Source/examples/unreachable-code.cpp
+++ Source/examples/unreachable-code.cpp
@@ -1,18 +1,12 @@
 // Copyright (c) 2014, Ruslan Baratov
 // All rights reserved.
 
-int main() {
-#include <leathers/push>
-#include <leathers/constant-conditional>
-  if (true) {
-    return 1;
-  }
-#include <leathers/pop>
-
 #include <leathers/push>
 #if !defined(SHOW_WARNINGS)
-# include <leathers/unreachable-code>
+# include <leathers/unreachable-code-simple>
 #endif
+int main() {
   return 2;
-#include <leathers/pop>
+  return 1;
 }
+#include <leathers/pop>
